9969d56b6edefaf4efb3f2d12108527270d07782
Thineshkumar S <53859752+Thineskumar-S@users.noreply.github.com>
2022-11-22 00:56:15 +0530

    edited the logic condition for divisible by 100
    According to the logic condition edited the code to year%100!=0

--- Leap year function file.py
+++ Leap year function file.py
@@ -1,7 +1,7 @@
 def is_leap(year):
     if 1900<= year<10**5:
         a=year%4==0
-        b=year%100==0 
+        b=year%100!=0 
         c=year%400==0
         if a and b and c:
             return True
@@ -11,4 +11,4 @@ def is_leap(year):
 
 year = int(input())
 print(is_leap(year))
-#logic is, a leap year can be divible by 4,100, 200 and whose reminder is 0
+#logic is, a leap year can be divible by 4,200 and not divisible by 100  with all this whose reminder is 0.

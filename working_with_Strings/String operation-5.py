abd2ba5b9747344f4ee2c3967fc24f6efb8470ac
Thineshkumar S <53859752+Thineskumar-S@users.noreply.github.com>
2023-08-01 11:13:20 +0530

    This is 1st rough code
    Need to evaluate the code according to the test conditons.

---
+++ working_with_Strings/String operation-5.py
@@ -0,0 +1,28 @@
+"""
+Joyal was given a sentence. His task is to delete the two words that comes together and print the sentence so that the words in the output sentence have distinct words compared to their adjacent words. If no words are present in the output sentence print -1
+
+Input Description:
+You are given input string 'S'
+
+Output Description:
+Print the all the words that are left in the string 's' so that the words in the output sentence have distinct words compared to their adjacent words. Print -1 if no words are left
+
+Sample Input :
+I am john cena cena john
+Sample Output :
+I am
+"""
+
+
+
+def find_Adj(phrase):
+    words=[i for i in sentence.split(" ")]
+    length=len(words)
+    for i in range(length-1):
+        if words[i]==words[i+1]:
+            for j in range(i):
+                print(words[j],end=" ")
+        else:
+            return -1
+sentence=input()
+print(find_Adj(sentence))
